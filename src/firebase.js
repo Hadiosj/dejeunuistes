8e88a84f77895e857a6fc67923047a5eb5b0f21d
Hadiosj <hadijaber30@hotmail.fr>
2026-02-14 22:35:42 +0100

    move firebase to .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// PASTE YOUR FIREBASE CONFIG HERE
 const firebaseConfig = {
-  apiKey: "AIzaSyBlD9MMnTtBfS9-XiUyAQajWDvaY4X0w5o",
-  authDomain: "pixel-resto-paris.firebaseapp.com",
-  projectId: "pixel-resto-paris",
-  storageBucket: "pixel-resto-paris.firebasestorage.app",
-  messagingSenderId: "91229551048",
-  appId: "1:91229551048:web:6ab11f4a346fa17b7da8fd"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
